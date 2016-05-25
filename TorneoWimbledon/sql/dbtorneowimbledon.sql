754f7717c7816b99406ed27072c0e5925d7122f7
FABAME <FABAME@FABAME>
2016-05-24 20:06:17 -0500

    El proyecto se ha generado correctamente.

---
+++ TorneoWimbledon/sql/dbtorneowimbledon.sql
@@ -0,0 +1,4 @@
+drop database if exists dbtorneowimbledon;
+
+create database dbtorneowimbledon;
+use dbtorneowimbledon;
---
+++ TorneoWimbledon/src/java/hibernateutil/HibernateUtil.java
@@ -0,0 +1,29 @@
+package hibernateutil;
+
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.SessionFactory;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+
+    public HibernateUtil() {
+
+    }
+
+    static {
+        try {
+            // Create the SessionFactory from standard (hibernate.cfg.xml) 
+            // config file.
+            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            // Log the exception. 
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
